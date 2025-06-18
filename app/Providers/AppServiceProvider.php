2c51f66f5097d6c8865a66de3d23242d0541d2b0
Lauro Guedes <leowgweb@gmail.com>
2025-06-18 18:53:32 +0100

    Add Task and Label models with migration, factory, and seeder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configModels();
+    }
+
+    private function configModels(): void
+    {
+        Model::unguard();
+        Model::shouldBeStrict();
     }
 }
